9eeb2a413362834d50dbe595694951de0d1b1d76
AekdyCoin <chenh1989@gmail.com>
2013-06-08 09:17:17 +0800

    Update PowMod.cpp

--- PowMod.cpp
+++ PowMod.cpp
@@ -3,7 +3,7 @@
  * $a or $b may be negative, however $c must be non-negative
  */
 template<class T> T PowMod( T a, T b, T c) {
-   T r=Mod(a,c);
+   	T r=Mod(a,c);
 	while(b != 0) {
 		if(b & 1) r=MulMod(r, a, c);
 		a = MulMod( a, a, c);
