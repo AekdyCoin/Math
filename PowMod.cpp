2fdb51164e6a525380e9e3328bce5d60f833265c
AekdyCoin <chenh1989@gmail.com>
2013-06-08 09:17:02 +0800

    Update and rename a^b mod c.cpp to PowMod.cpp

---
+++ PowMod.cpp
@@ -0,0 +1,13 @@
+/*
+ * Must guarantee c+c DO NOT OVERFLOW!!!(both a, b, c are INTEGERS)
+ * $a or $b may be negative, however $c must be non-negative
+ */
+template<class T> T PowMod( T a, T b, T c) {
+   T r=Mod(a,c);
+	while(b != 0) {
+		if(b & 1) r=MulMod(r, a, c);
+		a = MulMod( a, a, c);
+		b >>= 1;
+	}
+	return r;
+}
--- a^b mod c.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * Must guarantee c+c DO NOT OVERFLOW!!!(both a, b, c are INTEGERS)
- */
-template< class T> T PowMod( T a, T b, T c) {
-   
-}
