9429366ffd6293eb18fc518bf6e7b2fb0035b547
AekdyCoin <chenh1989@gmail.com>
2013-06-08 10:17:11 +0800

    Update Math.head.cpp

--- Math.head.cpp
+++ Math.head.cpp
@@ -1,4 +1,3 @@
-
 typedef long long LL ;
 
 /* a Mod b
@@ -116,3 +115,17 @@ template<class T> void FastFactorization(T num){
 	FastFactorization(num/factor);
 }
 
+/*
+ * solve ax=b(mod n)
+ * $x is the smallest solution.
+ * function return pair< # of solution, x >
+ * */
+
+template<class T>
+pair< T, T > SolveModLinearEquation( T a, T b, T n) {
+	T g,x,y,e,i,gap;
+	g=EXT_GCD(a,n,x,y);
+	if(b%g)return make_pair( 0, 0) ;
+	e=(x*(b/g)%n+n)%n; gap=n/g;
+	return make_pair(g, e-(e/gap)*gap);
+}
