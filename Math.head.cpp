0a0ab83ba48b9c471a03dd05df2f3f6a5382c226
AekdyCoin <chenh1989@gmail.com>
2013-06-08 09:22:43 +0800

    Update Math.head.cpp

--- Math.head.cpp
+++ Math.head.cpp
@@ -1,10 +1,9 @@
-
 /* a Mod b
  * $a could be negative, $b have to be positive integer
  */
  
 template<class T> T Mod( T a, T b) {
-  a%=b;
+  	a%=b;
 	return a<0?a+b:a;
 }
 
@@ -14,8 +13,8 @@ template<class T> T Mod( T a, T b) {
  
 template<class T> T MulMod( T a, T b, T c) {
 	T r=0;
-  a=Mod(a,c);
-  b=Mod(b,c);
+	a=Mod(a,c);
+	b=Mod(b,c);
 	while(b != 0) {
 		if(b & 1) if((r += a) >= c) r-= c;
 		if( (a<<=1) >= c) a-= c;
